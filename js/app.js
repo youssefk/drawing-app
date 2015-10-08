8e5801c2c5d7d4494b49dbe9a967b55e95a6ccd7
youssefk <youssefkhalifa@live.com>
2015-10-07 19:51:25 -0700

    Add planning documentation

---
+++ js/app.js
@@ -0,0 +1,20 @@
+//Problem: No user interaction causes no change to application
+//Solution: When user interacts, cause changes appropriately
+
+//When clicking on control list items
+  //Deselect sibling elements
+  //Select clicked element
+
+
+//When new color is pressed
+  //Show color select or hide the color select
+
+//When color sliders change
+  //Update the new color span
+
+//When add color is pressed
+  //Append the color to the controls ul
+  //Select the new color
+
+//On mouse events on the canvas
+  //Draw lines
