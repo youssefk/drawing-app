3d755b451efb60b912627d631b6b8190a42322d1
youssefk <youssefkhalifa@live.com>
2015-10-07 20:57:07 -0700

    Add preliminary drawing functionality

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts, cause changes appropriately
 var color = $(".selected").css("background-color");
+var contex = $("canvas")[0].getContext("2d");
 
 //When clicking on control list items
 $(".controls").on("click", "li", function() {
@@ -41,4 +42,12 @@ $("#addNewColor").click(function() {
 });
 
 //On mouse events on the canvas
+
   //Draw lines
+  context.beginPath();
+  context.moveTo(10, 10);
+  context.lineTo(20, 10);
+  context.lineTo(20, 20);
+  context.lineTo(10, 20);
+  context.closePath();
+  context.stroke();
