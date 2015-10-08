75704c18861d61c7c2b2efbd598a4295705dc4e8
youssefk <youssefkhalifa@live.com>
2015-10-07 20:05:10 -0700

    Add click controls to color picker

--- js/app.js
+++ js/app.js
@@ -1,10 +1,16 @@
 //Problem: No user interaction causes no change to application
 //Solution: When user interacts, cause changes appropriately
+var color = $(".selected").css("background-color");
 
 //When clicking on control list items
+$(".controls li").click(function() {
   //Deselect sibling elements
+  $(this).siblings().removeClass("selected");
   //Select clicked element
-
+  $(this).addClass("selected");
+  //Cache current color
+  color = $(this).css("background-color");
+});
 
 //When new color is pressed
   //Show color select or hide the color select
