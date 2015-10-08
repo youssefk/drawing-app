6c2850616089ae159d4d11d95c6c58c9bb375d75
youssefk <youssefkhalifa@live.com>
2015-10-07 20:48:07 -0700

    Add color adding functionality

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 var color = $(".selected").css("background-color");
 
 //When clicking on control list items
-$(".controls li").click(function() {
+$(".controls").on("click", "li", function() {
   //Deselect sibling elements
   $(this).siblings().removeClass("selected");
   //Select clicked element
@@ -31,8 +31,14 @@ function changeColor() {
   $("input[type=range]").change(changeColor);
 
 //When "Add Color" is pressed
+$("#addNewColor").click(function() {
+  var $newColor = $("<li></li>");
   //Append the color to the controls ul
+  $newColor.css("background-color", $("#newColor").css("background-color"));
+  $(".controls ul").append($newColor);
   //Select the new color
+  $newColor.click();
+});
 
 //On mouse events on the canvas
   //Draw lines
